4d0dab535d19563568dad64ed415175c99b3e180
Haonan Zhang <haonanz@stof01mcwk02.corp.curacloud.net>
2020-02-14 11:24:43 -0800

    commit

---
+++ src/main/java/com/example/stock_project_I/StockProjectIApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_project_I;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProjectIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockProjectIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stock_project_I/StockProjectIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_project_I;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProjectIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
