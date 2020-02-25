34bf1e0879b4bcbbf335504f927528a942ebd6d2
Haonan Zhang <haonanz@stof01mcwk02.corp.curacloud.net>
2020-02-24 16:31:55 -0800

    form validation

--- src/main/java/com/example/stock_project_I/GreetingController.java
+++ src/main/java/com/example/stock_project_I/GreetingController.java
@@ -1,23 +1,34 @@
 package com.example.stock_project_I;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class GreetingController {
+public class GreetingController implements WebMvcConfigurer{
 
-	@GetMapping("/greeting")
-	public String greetingForm(Model model) {
-		model.addAttribute("greeting", new Greeting());
-		return "greeting";
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/results").setViewName("results");
 	}
 
-	@PostMapping("/greeting")
-	public String greetingSubmit(@ModelAttribute Greeting greeting) {
-		return "result";
+	@GetMapping("/")
+	public String showForm(PersonForm personForm) {
+		return "form";
 	}
 	
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return "form";
+		}
+
+		return "redirect:/results";
+	}
 }
